9dbeb8f8940010cffdfa669516dcf1fad7b1dc02
Md Asifuzzman <faysal.12b@gmail.com>
2025-09-05 12:02:12 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // আপনার অন্যান্য কনফিগারেশন এখানে থাকতে পারে
-  experimental: {
-    // Firebase Studio-তে Cross Origin নোটিশটি ঠিক করার জন্য
-    allowedOrigins: ["*.cloudworkstations.dev"],
-  },
 };
 
 module.exports = nextConfig;
